8d6694b65b39f24828fa0d6d4de92f9675c184b7
Felipe <luis.velasquez01@correo.usa.edu.co>
2020-09-18 15:32:35 -0500

    Clase circulo terminada

--- Circulo.py
+++ Circulo.py
@@ -1,4 +1,4 @@
-
+import math
 from Punto import Punto
 class Circulo:
   def __init__(self,Punto,radio):
@@ -12,15 +12,26 @@ class Circulo:
             result=True
         else :
             result=False
-           
         return result
     
+  def hallarPerimetro(self):
+        perimetro=2*math.pi*self.radio
+        return perimetro
+    
+
+  def hallarArea(self):
+        area=math.pi*math.pow(self.radio,2)
+        return area
+    
+
+
 
 centro=Punto(2,3)
 r=4
 c1=Circulo(centro,r)
 p2=Punto(-2,3)
-print(str(c1.determinarDentroCirculo(p2)))
+print((c1.hallarPerimetro()))
+print((c1.hallarArea()))
 
 
 
